8cf2094cc5903b4b06657d24eaeb16dcf809291b
Chiêu Minh Tâm <minhtam.seta@gmail.com>
2017-02-28 15:25:17 +0700

    Use javascript ES 6

---
+++ utilities/math-utilities.js
@@ -0,0 +1,10 @@
+function generateRandom() {
+    return Math.random();
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+//export { generateRandom, sum }
+export { generateRandom as random, sum as sum}
